275693e64f5f68955acfd3e257f74bf5372293bd
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-01-07 20:10:45 -0500

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 a = 97
-while a <= 122:
-     print("{:c}".format(a), end="")
+while a <= 122 :
+     if a != 101 and a != 113:
+          print("{:c}".format(a), end="")
      a += 1
