30cd879df8cc4c817d5dd55ea8d1a47597f798c8
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-01-18 23:40:11 -0500

    Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -15,7 +15,7 @@ class Square:
     def size(self, value):
         if type(value) is not int:
             raise TypeError("size must be an integer")
-       elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
     def area(self):
