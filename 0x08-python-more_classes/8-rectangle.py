258014252189b2b3c385e72445ed66c8e1852cde
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-01-25 00:00:01 -0500

    Compare rectangles

---
+++ 0x08-python-more_classes/8-rectangle.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+"""Defines a class Rectangle"""
+
+
+class Rectangle:
+
+        """ Instantiation class Rectangle """
+
+        number_of_instances = 0
+        print_symbol = "#"
+
+        def __init__(self, width=0, height=0):
+
+                self.width = width
+                self.height = height
+                Rectangle.number_of_instances += 1
+
+        def __del__(self):
+                """prints when omegalul is deleted"""
+                print("Bye rectangle...")
+                Rectangle.number_of_instances -= 1
+
+        """ to Make a value private """
+        @property
+        def width(self):
+                return self.__width
+
+        """ get value private """
+        @width.setter
+        def width(self, value):
+                if type(value) is not int:
+                        raise TypeError("width must be an integer")
+
+                if value < 0:
+                        raise ValueError("width must be >= 0")
+                self.__width = value
+
+        """ to Make a value private """
+        @property
+        def height(self):
+                return self.__height
+
+        """ get value private """
+        @height.setter
+        def height(self, value):
+                if type(value) is not int:
+                        raise TypeError("height must be an integer")
+
+                if value < 0:
+                        raise ValueError("height must be >= 0")
+                self.__height = value
+
+        """ get area """
+        def area(self):
+                area = self.width * self.height
+                return area
+
+        """ get perimeter """
+        def perimeter(self):
+
+                if self.width == 0 or self.height == 0:
+                        Perimeter = 0
+                        return Perimeter
+
+                else:
+                        Perimeter = ((2 * self.width) + (2 * self.height))
+                        return Perimeter
+
+        """ str # """
+        def __str__(self):
+                string = ""
+                if self.__width != 0 and self.__height != 0:
+                   string += "\n".join(str(self.print_symbol) * self.__width
+                                       for j in range(self.__height))
+                return string
+
+        """ repr """
+        def __repr__(self):
+            return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+
+        @staticmethod
+        def bigger_or_equal(rect_1, rect_2):
+                """biggest omegalul based on area"""
+                if type(rect_1) is not Rectangle:
+                  raise TypeError("rect_1 must be an instance of Rectangle")
+                if type(rect_2) is not Rectangle:
+                  raise TypeError("rect_2 must be an instance of Rectangle")
+                if rect_2.area() > rect_1.area():
+                        return rect_2
+                return rect_1
