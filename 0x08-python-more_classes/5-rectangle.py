f1b6e0b01d6c84df8c022ead35e485f4d6ffd5d6
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-01-24 23:04:13 -0500

    Detect instance deletion

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -62,7 +62,7 @@ class Rectangle:
         if self.__width != 0 and self.__height != 0:
             string += "\n".join("#" * self.__width
                                 for j in range(self.__height))
-            return string
+        return string
 
     """ repr """
     def __repr__(self):
