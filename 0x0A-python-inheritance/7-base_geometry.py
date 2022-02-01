4e46addaa53ee5b1fb84fdaab9c2b69717411273
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-02-01 11:16:38 -0500

    Geometry

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -3,14 +3,14 @@
 
 
 class BaseGeometry:
-        """omegalul empty"""
-        def area(self):
-                """omegalul empty"""
-                raise Exception("area() is not implemented")
+"""omegalul empty"""
+    def area(self):
+            """omegalul empty"""
+        raise Exception("area() is not implemented")
 
-        def integer_validator(self, name, value):
+    def integer_validator(self, name, value):
         """omegalul empty"""
-            if type(value) is not int:
-                    raise TypeError("{:s} must be an integer".format(name))
-            if value <= 0:
-                    raise ValueError("{:s} must be greater than 0".format(name))
+        if type(value) is not int:
+                raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+                raise ValueError("{:s} must be greater than 0".format(name))
