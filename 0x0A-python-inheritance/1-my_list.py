61fe04c02b6907beac159ce763791ed94b56d33a
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-01-31 20:55:45 -0500

    My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,9 +6,9 @@ classes
 class MyList(list):
         """ Class My class """
         def print_sorted(self):
-        """
+            """
             this function copy list and print sorted values
             Args:
                 self: Mylist
-        """         
+            """         
             print(sorted(self))
