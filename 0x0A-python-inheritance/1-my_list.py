b96646937d95a54bf49f792433d8f0c99a1c6740
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-01-31 20:42:39 -0500

    My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,15 @@
+#!/usr/bin/python3
+
+"""
+classes
+    MyList
+"""
 class MyList(list):
     """ Class My class """
         def print_sorted(self):
+        """
+            this function copy list and print sorted values
+            Args:
+                self: Mylist
+        """         
             print(sorted(self))
