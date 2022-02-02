2ca18ab2338e5bcce311d75206c51f6b8c6820bd
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-02-01 23:14:25 -0500

    Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -6,9 +6,9 @@ import sys
 
 size = 0
 status_code = {"200": 0, "301": 0,
-                              "400": 0, "401": 0,
-                              "403": 0, "404": 0,
-                              "405": 0, "500": 0}
+               "400": 0, "401": 0,
+               "403": 0, "404": 0,
+               "405": 0, "500": 0}
 cont = 0
 try:
     for line in sys.stdin:
@@ -22,7 +22,7 @@ try:
                 size += int(splits[-1])
                 if aux == cont:
                     cont += 1
-            except:
+            except Exceptions:
                 if aux == cont:
                     continue
         if cont % 10 == 0:
@@ -34,7 +34,6 @@ try:
     for key, value in sorted(status_code.items()):
         if value:
             print("{:s}: {:d}".format(key, value))
-            
 except KeyboardInterrupt:
     print("File size: {:d}".format(size))
     for key, value in sorted(status_code.items()):
