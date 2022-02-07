8572ed84aa2d8a684e8acae9aa4d831c1f528d84
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-02-07 12:50:04 -0500

    Models

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Class Base """
+
+
+class Base:
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ constructor of the class Base """
+        if id is not None:
+            self.id = id
+
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+                                                                           
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3
+""" subClass Rectangle """
+from models.base import Base
+
+
+class Rectangle(Base):
+
+    __nb_objects = 0
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ constructor of the class Base """
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """Base class"""
+        return self.__width
+        
+    @property
+    def height(self):
+        """Base class"""
+        return self.__height
+
+    @property
+    def x(self):
+        """Base class"""
+        return self.__x
+
+    @property
+    def y(self):
+        """Base class"""
+        return self.__y
+
+
+    """ get value private """
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+
+        if value <= 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+
+    """ get value private """
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        """Base class"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        """Base class"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value   
+
+    def area(self):
+        return (self.__width * self.__height)
+    
+    def display(self):
+        print(("\n" * self.__y) + "\n".join(((" " * self.__x) +
+                                             ("#" * self.__width))
+                                            for i in range(self.__height)))
+
+    def __str__(self):
+        """Base class"""
+        return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format(self.id,
+                                                                 self.__x,
+                                                                 self.__y,
+                                                                 self.__width,
+                                                                 self.__height)
+
+    def update(self, *args):
+        self.id = id
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    
