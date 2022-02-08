721b090a15716848c3cc98ded548da3370e0e503
Artemisse99 <79029943+Artemisse99@users.noreply.github.com>
2022-02-07 22:47:37 -0500

    Update base.py
    Identation whit python idle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,76 +1,123 @@
+
 #!/usr/bin/python3
-""" Class Base """
+"""created class"""
+import csv
 import json
-from os import path
 
 
 class Base:
 
+    """class attribute, private"""
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """ constructor of the class Base """
-        if id is not None:
-            self.id = id
+        """class constructor, instance attributes"""
 
+        if id is not None:
+            """asign  instance attribute, public"""
+
+            self.id = id
         else:
+            """increment attribute of class"""
+
             Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+            """and assign the new value of __nb...
+            to the instance atrribute, public id"""
+
+            self.id = self.__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Using file"""
-        if list_dictionaries is None or len(list_dictionaries) is 0:
+        """@staticmethod or class.function = staticmethod(class.function)"""
+
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
-        return json.dumps(list_dictionaries)
+        else:
+            return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """Using file"""
-        list_dictionaries = []
-        if list_objs is None:
-            with open(cls.__name__ + ".json", "w",  encoding='utf-8') as file:
-                file.write(Base.to_json_string(list_dictionaries))
-            return
-        for model in list_objs:
-            list_dictionaries.append(model.to_dictionary())
-            with open(cls.__name__ + ".json", "w",  encoding='utf-8') as file:
-                file.write(Base.to_json_string(list_dictionaries))
+        """writes the JSON string representation"""
+
+        tmp = []
+        tmptwo = "{}.json".format(cls.__name__)
+        if list_objs is not None:
+            for i in range(len(list_objs)):
+                tmp.append(cls.to_dictionary(list_objs[i]))
+        with open(tmptwo, mode='w', encoding="utf-8") as json_file:
+            json_file.write(cls.to_json_string(tmp))
 
     @staticmethod
     def from_json_string(json_string):
-        """Using Jason"""
-        if json_string is None:
+        """stactic method  that returb lists  of the json string"""
+
+        if json_string is None or json_string == '':
             return []
-        return json.loads(json_string)
+        else:
+            return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
-        """Using file"""
+        """function create method"""
         if cls.__name__ == "Rectangle":
-            object = cls(1, 1)
-            object.update(**dictionary)
-            return object
+            tmp_create = cls(4, 5)
+        else:
+            tmp_create = cls(6)
 
-        if cls.__name__ == "Square":
-            object = cls(1)
-            object.update(**dictionary)
-            return object
+        tmp_create.update(**dictionary)
+        return tmp_create
 
     @classmethod
     def load_from_file(cls):
-        """using file"""
-        if path.exists(cls.__name__ + ".json") is False:
+        """function load method"""
+
+        tmp_load = cls.__name__ + '.json'
+        try:
+            tmp_file = []
+            with open(tmp_load, mode='r', encoding='utf-8') as tmp_from_file:
+                tmp_three = Base.from_json_string(tmp_from_file.read())
+                for i in tmp_three:
+                    tmp_file += [cls.create(**i)]
+                return tmp_file
+        except:
             return []
-        with open(cls.__name__ + ".json", "r",  encoding='utf-8') as file:
-            listofinstances = []
-            objectlist = cls.from_json_string(file.read())
-            for dict in objectlist:
-                objectdict = {}
-                for key, value in dict.items():
-                    objectdict[key] = value
-                    listofinstances.append(cls.create(**objectdict))
-            return listofinstances
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """"""
+
+        tmp_save = '{}.csv'.format(cls.__name__)
+        with open(tmp_save, mode='w+', newline='') as tmp_file_saves:
+            if list_objs is None or list_objs == []:
+                tmp_file_saves.write('[]')
+            else:
+                if cls.__name__ == 'Square':
+                    tm = ['id', 'size', 'x', 'y']
+                else:
+                    tm = ['id', 'width', 'height', 'x', 'y']
+                t_csv = csv.DictWriter(tmp_file_saves, fieldnames=tm)
+                ty = []
+                for j in list_objs:
+                    ty += [t_csv.writerow(j.to_dictionary())]
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """create load method"""
+
+        tmp_save = '{}.csv'.format(cls.__name__)
+        try:
+            with open(tmp_save, mode='r', newline='') as tmp_file_saves:
+                if cls.__name__ == 'Square':
+                    tm = ['id', 'size', 'x', 'y']
+                else:
+                    tm = ['id', 'width', 'height', 'x', 'y']
+                t_csv = csv.DictReader(tmp_file_saves, fieldnames=tm)
+                tys = [{key: int(value) for key, value in dict.items()}
+                       for dict in t_csv]
+                return [cls.create(**dics) for dics in tys]
+        except IOError:
+            return []
 
     @classmethod
     def reset(cls):
