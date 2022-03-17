bdc22ea05b38295f1d45761120dd738d904d545c
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-03-17 12:53:19 -0500

    Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,4 +2,4 @@
 -- because Batch 3 is the best!
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY user_0d_2_pwd;
 CREATE IF NOT EXISTS DATABASE hbtn_0d_2;
-GRANT SELECT ON hbtn_0d_2 TO user_0d_2@localhost;
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
