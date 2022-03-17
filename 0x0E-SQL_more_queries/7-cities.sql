0ea7f492d23b0c2d28296ad9cc2626919aa5635f
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-03-17 12:46:11 -0500

    Cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,8 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-CREATE TABLE IF NOT EXISTS cities(
-id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-state_id INT NOT NULL FOREIGN KEY,
-name VARCHAR(256) NOT NULL    
-);
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+state_id INT NOT NULL,
+name VARCHAR(256) NOT NULL,
+FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id));
