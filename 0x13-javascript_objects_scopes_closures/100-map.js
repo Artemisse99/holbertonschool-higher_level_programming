831e7e5eb539d3ac18b29fb61983edce3dc9b5c1
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-04-07 12:33:27 -0500

    [100]

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list = require('./100-map');
+const list = require('./100-data').list;
 console.log(list);
 const roots = list.map((num) => Math.sqrt(num));
 console.log(roots);
