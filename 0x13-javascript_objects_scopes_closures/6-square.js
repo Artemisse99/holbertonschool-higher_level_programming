370ed24a8bff4460c76d6acdf9d5510db11114cb
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-04-07 12:07:21 -0500

    [Update-6]

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,31 +1,5 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (h, w) {
-    if (h > 0 && w > 0) {
-      this.height = h;
-      this.width = w;
-    } else {
-      return Rectangle.removeClass;
-    }
-  }
-
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('x'.repeat(this.height));
-    }
-  }
-
-  rotate () {
-    const temp = this.width;
-    this.width = this.height;
-    this.height = temp;
-  }
-
-  double () {
-    this.height *= 2;
-    this.width *= 2;
-  }
-}
+const Rectangle = require('./4-rectangle');
 module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
