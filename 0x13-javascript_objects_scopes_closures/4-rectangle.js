9821ea26df87ea0b07c76f4552222f8c73f148eb
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-04-07 10:40:19 -0500

    [Up-up]

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -11,7 +11,7 @@ module.exports = class Rectangle {
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      console.log('x'.repeat(this.height));
+      console.log('x'.repeat(this.width));
     }
   }
 
