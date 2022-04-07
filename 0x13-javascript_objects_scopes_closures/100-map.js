c55ee7534c2d40e76cb0b6ba40aaf33531522b45
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-04-07 12:45:43 -0500

    [100]

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let list = require('./100-data').list;
+const list = require('./100-data').list;
 console.log(list);
-let roots = list.map((num, index) => num * index);
+const roots = list.map((num, index) => num * index);
 console.log(roots);
