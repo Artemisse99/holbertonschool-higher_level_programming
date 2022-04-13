78a2d1ff3996d80fb0b99b88b5aefac34b24d593
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-04-12 22:32:42 -0500

    Web Scraping [xv]

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,18 @@
 #!/usr/bin/node
 const request = require('request');
-const fargs = process.argv;
-request(fargs[2], function (error, response, body) {
+const args = process.argv;
+const url = args[2];
+request(url, function (error, response, body) {
   if (error) {
     console.log(error);
-  }
-  let count = 0;
-  const js = JSON.parse(body).results;
-  for (let i = 0; i < js.length; i++) {
-    const character = js[i].characters.find((js) => {
-      return (js.match(/18/));
-    });
-    if (character !== undefined) {
-      count++;
+  } else {
+    const obj = JSON.parse(body);
+    const dictionary = {};
+    for (const keys in obj) {
+      if (obj[keys].completed === true) {
+        dictionary[obj[keys].userId] = dictionary[obj[keys].userId] + 1 || 1;
+      }
     }
+    console.log(dictionary);
   }
-  console.log(count);
 });
