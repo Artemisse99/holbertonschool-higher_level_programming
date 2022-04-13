bf224780d65dee626bde98e6a478a56b50f665f0
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-04-12 22:10:59 -0500

    Web Scraping [xv]

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,21 +1,18 @@
 #!/usr/bin/node
 const request = require('request');
-const resdic = { json: true };
-const results = {};
-request(process.argv[2], resdic, (error, res, body) => {
+const args = process.argv;
+const url = args[2];
+request(url, function (error, response, body) {
   if (error) {
-    return console.log(error);
-  }
-  if (!error && res.statusCode === 200) {
-    body.forEach(tasks => {
-      if (tasks.completed) {
-        if (!results[tasks.userId]) {
-          results[tasks.userId] = 1;
-        } else {
-          results[tasks.userId] += 1;
-        }
+    console.log(error);
+  } else {
+    const obj = JSON.parse(body);
+    const dictionary = {};
+    for (const keys in obj) {
+      if (obj[keys].completed === true) {
+        dictionary[obj[keys].userId] = dictionary[obj[keys].userId] + 1 || 1;
       }
-    });
-    console.log(results);
+    }
+    console.log(dictionary);
   }
 });
