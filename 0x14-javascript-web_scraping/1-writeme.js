2fe1d13bdf667bb7640ef96c73fbe16696885a28
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-04-12 19:26:10 -0500

    Web Scraping [XD]

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[2], (err, data) => {
+fs.writeFile(process.argv[2], process.argv[2], 'utf8', (err, data) => {
   if (err) throw err;
 });
