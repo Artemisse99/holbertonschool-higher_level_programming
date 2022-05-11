05c77c4f1c0e63a822e08fb4564e738867ebaf0f
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-05-10 21:09:26 -0500

    Update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa"""
-
+"""table of hbtn_0e_0_usa"""
 
 
 if __name__ == "__main__":
     import MySQLdb
-    import sys 
-    
+    import sys
+
     data = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3])
     state = data.cursor()
     state.execute("SELECT * FROM states WHERE name LIKE BINARY '{}%'; "
-                .format(sys.argv[4]))
+                  .format(sys.argv[4]))
     query_rows = state.fetchall()
     for row in query_rows:
         print(row)
