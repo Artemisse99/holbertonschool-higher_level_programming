eeca2f7521022ba58e434a6369309496ec043eaa
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-05-10 20:30:36 -0500

    Update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""script that lists all states with a name starting with N (upper N)"""
+"""Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa"""
 
+import MySQLdb
+from sys import argv
 if __name__ == "__main__":
 
-    import MySQLdb
-    from sys import argv
+    
 
     data = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3])
