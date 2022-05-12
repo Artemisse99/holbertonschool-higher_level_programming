768332b6b79941a1605cbff595b780e9a863cb37
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-05-12 14:14:04 -0500

    Update new

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,14 +6,17 @@ database hbtn_0e_0_usa"""
 if __name__ == "__main__":
     import MySQLdb
     import sys
-    data = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3])
+    data = MySQLdb.connect(host="localhost", 
+                           port=3306, 
+                           user=sys.argv[1],
+                           passwd=sys.argv[2], 
+                           db=sys.argv[3])
     state = data.cursor()
     state.execute("SELECT * FROM states WHERE name=%s \
         ORDER BY states.id ASC", (argv[4],))
     query_rows = state.fetchall()
     for row in query_rows:
         print(row)
-        
+
     state.close()
     data.close()
